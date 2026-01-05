63923273ab1d68ff3be73359b26cb93d4c96ed89
Isaac <isaclopezcar@gmail.com>
2026-01-05 14:48:33 -0300

    Resolucion task1-task5

--- task3.js
+++ task3.js
@@ -1,13 +1,19 @@
 // Type your code below this line!
 
 function Mail(subj, msg) {
-    this.subject = subj
-    this.message = msg
-    
+  this.subject = subj
+  this.message = msg
+
+  this.printMail = function () {
+    console.log(`${this.subject} : ${this.message}`);
+
   }
-  
-  const newMail = new Mail(  )
-  
-  // Type your code above this line!
-  
-  newMail.printMail()
+
+
+}
+
+const newMail = new Mail(process.argv[2], process.argv[3]);
+
+// Type your code above this line!
+
+newMail.printMail()
--- task5.js
+++ task5.js
@@ -2,6 +2,19 @@
 
 // Type your code below this line!
 
+function FriendList(num) {
+
+    this.friends = [];
+
+    for (let i = 0; i < num; i++) {
+        this.friends.push(process.argv[i + 3])
+    }
+
+}
+
+const number = Number(process.argv[2]);
+const listFriends = new FriendList(number);
+console.log(listFriends.friends);
 
 
 // Type your code above this line!
